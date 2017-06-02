16acf72ab6f8b52c21d53d1637ed4263b3ae6deb
praveenkaushik80 <praveen.kaushik@outlook.com>
2017-06-02 14:50:00 +0530

    Project
    adding Project Files

---
+++ Project/run_analysis.r
@@ -0,0 +1,5 @@
+require(knitr)
+require(markdown)
+setwd("C:/Users/prave/Desktop/coursera/GCD/GettingAndCleaningData/Project")
+knit("run_analysis.Rmd", encoding="ISO8859-1")
+markdownToHTML("run_analysis.md", "run_analysis.html")
